8ac1fad1c0adcfe44c529fc231ecf694b3d6fc72
Michael Crouse <mcrouse911@gmail.com>
2017-07-03 20:58:18 -0400

    working site with buttons and bootstrap

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,19 @@ class WelcomeController < ApplicationController
   def index
   	@homeland = 'USA'
   	@countries = ['Switzerland', 'Russia', 'Germany']
+  	@countries_pics = ['Russia.jpeg', 'Canada.jpeg']
+  	
   end
 
-  def params
+  def about_params
+  	if params[:color] == nil
+  		@color = "orange"
+  	else
+  	@color = params[:color]
+  end
+
+  	@size = params[:shoe_size]
+
+
   end
 end
